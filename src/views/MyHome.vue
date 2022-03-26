5f2c0d15683a328ce150a1370b2ece992723f9e3
rodionbgd <rodionbgd@mail.ru>
2022-03-26 14:55:42 +0300

    feat: Стилизация главной страницы

--- src/views/MyHome.vue
+++ src/views/MyHome.vue
@@ -1,12 +1,32 @@
 <template>
-  <div>
-    <h1>Home</h1>
+  <div class="home">
+    <h1>All destinations</h1>
+    <div class="destinations">
+      <router-link
+        v-for="destination in destinations"
+        :key="destination.id"
+        :to="destination.slug"
+      >
+        <h2>
+          {{ destination.name }}
+        </h2>
+        <img :src="`/images/${destination.image}`" :alt="destination.slug" />
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
+import sourceData from "@/data.json";
+
 export default {
   name: "MyHome",
+
+  data() {
+    return {
+      destinations: sourceData.destinations,
+    };
+  },
 };
 </script>
 
