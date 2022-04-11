23d71f988b0b193cf83fd8c9028549ffaa6fe303
rodionbgd <rodionbgd@gmail.com>
2022-04-11 16:53:03 +0000

    Deploying to gh-pages from  @ 03245da58f86ea128baf003677d5b8d301dca276 🚀

---
+++ assets/TheInvoices.d93691b8.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.05b5019a.js";import{o,c as s,d as t}from"./vendor.b7a25862.js";const c={name:"TheInvoices"},n=t("h1",null,"Invoices",-1),r=[n];function a(_,i,d,p,l,f){return o(),s("div",null,r)}var u=e(c,[["render",a]]);export{u as default};
